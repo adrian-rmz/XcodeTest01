686961634085008d730e8bb020ab90101d8695f2
Adrián Alejandro Ramírez Cruz <adrian.rmz.587@gmail.com>
2024-11-04 15:21:04 +0100

    Initial Commit

---
+++ Test01/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Test01
+//
+//  Created by Adrián Alejandro Ramírez Cruz on 04/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Test01/Test01App.swift
@@ -0,0 +1,17 @@
+//
+//  Test01App.swift
+//  Test01
+//
+//  Created by Adrián Alejandro Ramírez Cruz on 04/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Test01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
